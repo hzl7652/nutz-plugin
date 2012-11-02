177be89c711fa26f6c240b0cf1b5120e86c784a7
wangcheng <feiyan35488@126.com>
2012-11-02 13:50:43 +0800

    plugin coc and scoffold

---
+++ src/net/tt64/nutz/plugin/scoffold/Scoffold.java
@@ -0,0 +1,15 @@
+package net.tt64.nutz.plugin.scoffold;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Scoffold {
+	
+}
